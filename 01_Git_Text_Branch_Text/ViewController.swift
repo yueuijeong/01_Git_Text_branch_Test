2434f3e83123a2f279222593d331aa84d0a2aed0
D7703_21 <d7703_21@d7703-21ui-imac.local>
2017-09-12 10:34:15 +0900

    add print

--- 01_Git_Text_Branch_Text/ViewController.swift
+++ 01_Git_Text_Branch_Text/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("1st commit")
+        print("1st commit")//
+        
     }
 
     override func didReceiveMemoryWarning() {
